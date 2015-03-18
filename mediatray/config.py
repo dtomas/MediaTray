93085f46bda6d7e5def26c6c096ddc4e967bb930
Dennis Tomas <dtomas@onegreenbyte.com>
2015-03-18 12:31:43 +0100

    Use xdg_config_home from rox-lib.

--- mediatray/config.py
+++ mediatray/config.py
@@ -1,14 +1,11 @@
 import os
 
+from rox.basedir import xdg_config_home
+
 
 SITE = "dtomas"
 PROJECT = "MediaTray"
 
-
-xdg_config_home = os.getenv("XDG_CONFIG_HOME")
-
-if xdg_config_home is None:
-    xdg_config_home = os.path.join(os.getenv("HOME"), ".config")
 
 config_dir = os.path.join(xdg_config_home, SITE, PROJECT)
 if not os.path.isdir(config_dir):
