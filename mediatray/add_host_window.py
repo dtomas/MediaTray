a65e7d6f6f0106f9b9b9ce3cbbed5466bb7c5ced
Dennis Tomas <dtomas@onegreenbyte.com>
2015-03-25 22:38:14 +0100

    Port selection.

--- mediatray/add_host_window.py
+++ mediatray/add_host_window.py
@@ -34,26 +34,32 @@ class AddHostWindow(gtk.Dialog):
         self.__hostname_input = gtk.Entry()
         self.__username_input = gtk.Entry()
         self.__path_input = gtk.Entry()
+        self.__port_select = gtk.SpinButton(gtk.Adjustment(0, 0, 65534, 1), 1)
 
         table.attach(gtk.Label(_("Protocol")), 0, 1, 0, 1)
         table.attach(gtk.Label(_("Hostname")), 0, 1, 1, 2)
         table.attach(gtk.Label(_("Username")), 0, 1, 2, 3)
-        table.attach(gtk.Label(_("Path")), 0, 1, 3, 4)
+        table.attach(gtk.Label(_("Port")), 0, 1, 3, 4)
+        table.attach(gtk.Label(_("Path")), 0, 1, 4, 5)
 
         table.attach(self.__protocol_select, 1, 2, 0, 1)
         table.attach(self.__hostname_input, 1, 2, 1, 2)
         table.attach(self.__username_input, 1, 2, 2, 3)
-        table.attach(self.__path_input, 1, 2, 3, 4)
-        
+        table.attach(self.__port_select, 1, 2, 3, 4)
+        table.attach(self.__path_input, 1, 2, 4, 5)
+
         def response(dialog, response_id):
             if response_id == gtk.RESPONSE_ACCEPT:
                 column = self.__protocol_select.get_property("active")
                 iter = self.__protocols_model.get_iter((column,))
-                uri = '{protocol}://{username}@{hostname}/{path}'.format(
-                    protocol=self.__protocols_model.get(iter, 1)[0],
+                protocol = self.__protocols_model.get(iter, 1)[0]
+                port = self.__port_select.get_value()
+                uri = '{protocol}://{username}@{hostname}{port}/{path}'.format(
+                    protocol=protocol,
                     username=self.__username_input.get_text(),
                     hostname=self.__hostname_input.get_text(),
                     path=self.__path_input.get_text(),
+                    port=':%s' % port if port != 0 else '',
                 )
                 self.__host_manager.add_host(uri)
             dialog.destroy()
