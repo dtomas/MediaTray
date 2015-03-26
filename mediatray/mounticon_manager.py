dfc410a265fc6905c42cd44e29370b412e74cb3a
Dennis Tomas <dtomas@onegreenbyte.com>
2015-03-26 06:57:34 +0100

    Check for file, not mount equality when mount is added.

--- mediatray/mounticon_manager.py
+++ mediatray/mounticon_manager.py
@@ -6,8 +6,11 @@ def manage_mounticons(tray, mounticon_config):
     volume_monitor = gio.volume_monitor_get()
 
     def mount_added(volume_monitor, mount):
+        mountpoint = mount.get_root()
         for icon in tray.icons:
-            if icon.get_mount() is mount:
+            if icon.get_mount() is None:
+                continue
+            if icon.get_mount().get_root() == mountpoint:
                 icon.mounted()
 
     class handlers:
