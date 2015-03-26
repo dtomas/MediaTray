ba27cd86b3b535e10b79e58abb7b51638351f9f1
Dennis Tomas <dtomas@onegreenbyte.com>
2015-03-26 09:23:52 +0100

    Error handling in HostEditor.

--- mediatray/host_editor.py
+++ mediatray/host_editor.py
@@ -39,6 +39,10 @@ class HostEditor(gtk.Dialog):
         self.__path_input = gtk.Entry()
         self.__port_select = gtk.SpinButton(gtk.Adjustment(0, 0, 65534, 1), 1)
 
+        self.__hostname_error = gtk.Label()
+        self.__port_error = gtk.Label()
+        self.__protocol_error = gtk.Label()
+
         if self.__host is not None:
             self.__hostname_input.set_text(self.__host.hostname)
             self.__username_input.set_text(self.__host.username)
@@ -52,25 +56,50 @@ class HostEditor(gtk.Dialog):
                     break
                 iter = self.__protocols_model.iter_next(iter)
 
-        table.attach(gtk.Label(_("Protocol")), 0, 1, 0, 1)
-        table.attach(gtk.Label(_("Hostname")), 0, 1, 1, 2)
-        table.attach(gtk.Label(_("Username")), 0, 1, 2, 3)
-        table.attach(gtk.Label(_("Port")), 0, 1, 3, 4)
-        table.attach(gtk.Label(_("Path")), 0, 1, 4, 5)
+        table.attach(self.__protocol_error, 0, 2, 0, 1)
+        table.attach(gtk.Label(_("Protocol")), 0, 1, 1, 2)
+        table.attach(self.__hostname_error, 0, 2, 2, 3)
+        table.attach(gtk.Label(_("Hostname")), 0, 1, 3, 4)
+        table.attach(gtk.Label(_("Username")), 0, 1, 4, 5)
+        table.attach(gtk.Label(_("Port")), 0, 1, 5, 6)
+        table.attach(gtk.Label(_("Path")), 0, 1, 6, 7)
 
-        table.attach(self.__protocol_select, 1, 2, 0, 1)
-        table.attach(self.__hostname_input, 1, 2, 1, 2)
-        table.attach(self.__username_input, 1, 2, 2, 3)
-        table.attach(self.__port_select, 1, 2, 3, 4)
-        table.attach(self.__path_input, 1, 2, 4, 5)
+        table.attach(self.__protocol_select, 1, 2, 1, 2)
+        table.attach(self.__hostname_input, 1, 2, 3, 4)
+        table.attach(self.__username_input, 1, 2, 4, 5)
+        table.attach(self.__port_select, 1, 2, 5, 6)
+        table.attach(self.__path_input, 1, 2, 6, 7)
+
+        def clear_errors():
+            self.__protocol_error.hide()
+            self.__hostname_error.hide()
 
         def response(dialog, response_id):
+            clear_errors()
             if response_id == gtk.RESPONSE_ACCEPT:
+                has_error = False
+                hostname = self.__hostname_input.get_text()
+                if not hostname:
+                    has_error = True
+                    self.__hostname_error.set_text(
+                        _("You must enter a hostname.")
+                    )
+                    self.__hostname_error.show()
                 column = self.__protocol_select.get_property("active")
-                iter = self.__protocols_model.get_iter((column,))
-                protocol = self.__protocols_model.get(iter, 1)[0]
-                username = self.__username_input.get_text()
+                try:
+                    iter = self.__protocols_model.get_iter((column,))
+                except ValueError:
+                    has_error = True
+                    self.__protocol_error.set_text(
+                        _("You must select a protocol.")
+                    )
+                    self.__protocol_error.show()
+                else:
+                    protocol = self.__protocols_model.get(iter, 1)[0]
+                if has_error:
+                    return
                 port = self.__port_select.get_value()
+                username = self.__username_input.get_text()
                 uri = '{protocol}://{username}{hostname}{port}/{path}'.format(
                     protocol=protocol,
                     username='%s@' % username if username else '',
@@ -85,3 +114,4 @@ class HostEditor(gtk.Dialog):
             dialog.destroy()
         self.connect("response", response)
         self.show_all()
+        clear_errors()
