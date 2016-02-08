fd0f1f4bd8a1a21f62681d539f9e4a573bfeea26
Dennis Tomas <den.t@gmx.de>
2016-02-08 21:17:06 +0100

    Comment about 0=default port.

--- mediatray/host_editor.py
+++ mediatray/host_editor.py
@@ -61,7 +61,7 @@ class HostEditor(gtk.Dialog):
         table.attach(self.__hostname_error, 0, 2, 2, 3)
         table.attach(gtk.Label(_("Hostname")), 0, 1, 3, 4)
         table.attach(gtk.Label(_("Username")), 0, 1, 4, 5)
-        table.attach(gtk.Label(_("Port")), 0, 1, 5, 6)
+        table.attach(gtk.Label(_("Port (0=default)")), 0, 1, 5, 6)
         table.attach(gtk.Label(_("Path")), 0, 1, 6, 7)
 
         table.attach(self.__protocol_select, 1, 2, 1, 2)
